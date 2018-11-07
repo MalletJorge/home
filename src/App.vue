b8beefd03c29ca0c274d7e5f5de5f711142e2705
Lays Rodrigues <lays.rodrigues@kde.org>
2018-11-07 16:35:58 -0200

    Import timeline component to App
    Signed-off-by: Lays Rodrigues <lays.rodrigues@kde.org>

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app dark>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Lays Rodrigues - </span>
+        <span class="font-weight-light">Software Developer</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+
+    <v-content>
+      <Timeline></Timeline>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Timeline from './components/timeline'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  components:{
+    Timeline
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
