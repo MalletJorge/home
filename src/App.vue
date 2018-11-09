69696eeb5218db705db76305c39146f8477c6f73
Lays Rodrigues <lays.rodrigues@kde.org>
2018-11-09 02:13:07 -0200

    First version of projects
    Signed-off-by: Lays Rodrigues <lays.rodrigues@kde.org>

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,38 @@
 <template>
   <v-app dark>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Lays Rodrigues - </span>
-        <span class="font-weight-light">Software Developer</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-        <v-btn color="indigo">About Me</v-btn>
-        <v-btn color="indigo">Projects</v-btn>
-        <v-btn color="indigo">Timeline</v-btn>
-    </v-toolbar>
-    <v-content>
-      <v-layout column>
-        <AboutMe></AboutMe>
-        <Timeline id='timeline'></Timeline>
-      </v-layout>
-    </v-content>
+    <v-container>
+      <v-toolbar app>
+        <v-toolbar-title class="headline text-uppercase">
+          <span>Lays Rodrigues - </span>
+          <span class="font-weight-light">Software Developer</span>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items>
+            <v-btn flat>About Me</v-btn>
+            <v-btn flat>Projects</v-btn>
+            <v-btn flat>Timeline</v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+      <v-content>
+        <v-layout column>
+          <AboutMe></AboutMe>
+          <Timeline id='timeline'></Timeline>
+          <Projects></Projects>
+        </v-layout>
+      </v-content>
+    </v-container>
   </v-app>
 </template>
 
 <script>
 import Timeline from './components/timeline'
 import AboutMe from './components/aboutme'
+import Projects from './components/projects'
 export default {
   components:{
     Timeline,
-    AboutMe
+    AboutMe,
+    Projects
   }
 }
 </script>
---
+++ src/components/projects.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-container grid-list-xl>
+    <div class="headline">
+      Projects
+    </div>
+    <v-layout column>
+      <v-flex>
+        <v-card dark hover="true">
+          <v-card-title primary-title class="headline"> Atelier | #3DPrinting</v-card-title>
+          <v-card-text>
+            <p>
+              Atelier is a printer host for 3DPrinters. It allows you to manage and control
+              your 3DPrinter from your computer, and in the future from any device.
+              Atelier is built with Qt and KDE Frameworks. Using all the power of C++ to
+              bring the best experience to the user while keeping the standards of
+              Free Software that built the 3DPrinting community in the past years.
+            </p>
+            <p>
+              Find out more about Atelier:
+            </p>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn href="https://atelier.kde.org" target="_blank" flat>Website</v-btn>
+            <v-btn href="https://t.me/KDEAtelier" flat>Join on Telegram</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+      <v-flex>
+        <v-card dark hover="true">
+          <v-card-title primary-title class="headline">This | #VueJs </v-card-title>
+          <v-card-text>
+            <p>
+              This landing page is my new home. Used to learn a little more about VueJs.
+              Used Vuetify as framework for the components and layout.
+              Since my first web page, I though that I didn't like web development,
+              but every time that I have chance to do something better, I give it a try.
+              And VueJs its an amazing opportunity to do that.
+            </p>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex>
+        <v-card dark hover="true">
+          <v-card-title primary-title class="headline">Akademy 2018 Schedule </v-card-title>
+          <v-card-text>
+            <p>
+              Akademy is the international conference of KDE Community. And in 2018 it
+              happened at the beautiful city of Vienna/Austria. To support my fellow
+              friends of the community, I built a progressive web app using VueJs and Quasar Framework
+              so everyone could access a mobile-friendly version of the schedule.
+            </p>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn href="https://lays147.github.io/Akademy" target="_blank" flat>Website</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
