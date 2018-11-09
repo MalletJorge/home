d07f3b40147544775f5cde40d7819c64395b1473
Lays Rodrigues <lays.rodrigues@kde.org>
2018-11-09 01:24:07 -0200

    Update about me
    Signed-off-by: Lays Rodrigues <lays.rodrigues@kde.org>

--- src/components/aboutme.vue
+++ src/components/aboutme.vue
@@ -7,8 +7,8 @@
         <v-card-text>
           <h3>
             <strong>"Never give in, Never give up. 12th Doctor"</strong><br />
-              ~7 years in college. <br />~3 years going to events and knowing
-              what the world has to offer and discovering how I can use
+              ~7 years in college. <br />~3 years going to events and discovering
+              what the world has to offer and learning how I can use
               <strong>technology</strong> to change my life and the world.<br /> Below you can
               discover what are the adventures that I have in my life. Always
               using Doctor Who speaches to try to be the best person that I can.
