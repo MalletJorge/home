48cd5e2fbb433cd83590843c3492e321ef017172
Lays Rodrigues <lays.rodrigues@kde.org>
2018-11-09 23:38:08 -0200

    Add ganalytics
    Signed-off-by: Lays Rodrigues <lays.rodrigues@kde.org>

--- src/App.vue
+++ src/App.vue
@@ -44,3 +44,12 @@ export default {
   }
 }
 </script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-103612095-5"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-103612095-5');
+</script>
