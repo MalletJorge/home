93e87258f94ab282fb04cf8d4f57b4930ff285cf
Lays Rodrigues <lays.rodrigues@kde.org>
2018-11-11 15:06:03 -0200

    Use vue-analytics
    Signed-off-by: Lays Rodrigues <lays.rodrigues@kde.org>

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import './plugins/vuetify'
+import VueAnalytics from 'vue-analytics'
 import App from './App.vue'
 
 Vue.config.productionTip = false
@@ -7,3 +8,7 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+Vue.use(VueAnalytics, {
+  id: 'UA-103612095-5'
+})
